1c860dad042bbc762cbcf523c442bb74133cf801
apagani <apagani@tecniplast.it>
2019-09-23 20:58:51 +0100

    Integral index

--- sources/include/flic/IntegralIndex.hh
+++ sources/include/flic/IntegralIndex.hh
@@ -12,21 +12,78 @@
 #if !defined( FLIC_INTEGRAL_INDEX_HH )
 #define FLIC_INTEGRAL_INDEX_HH
 
+#include <flic/Option.hh>
+
+template<typename T>
+class IntegralIndex;
+
+template<typename T>
+bool operator==( IntegralIndex<T> const& lhs, IntegralIndex<T> const& rhs );
+
 template<typename T>
 class IntegralIndex
 {
     public:
         typedef T IndexedType;
 
-        IntegralIndex( T begin, T end, T step=1 )
+        IntegralIndex( T begin, T end, T step=static_cast<T>(1) );
 
         IntegralIndex<T> next() const;
         bool isValid() const;
         Option<T> get() const;
     private:
+        friend bool operator==<>( IntegralIndex<T> const&, IntegralIndex<T> const& );
+
         T m_current;
         T m_end;
         T m_step;
 };
+
+template<typename T>
+IntegralIndex<T>::IntegralIndex( T begin, T end, T step ) :
+                  m_current{begin},
+                  m_end{ end },
+                  m_step{ step }
+{}
+
+template<typename T>
+IntegralIndex<T> IntegralIndex<T>::next() const
+{
+    if( isValid() )
+    {
+        return IntegralIndex<T>{ m_current+m_step, m_end, m_step };
+    }
+    else
+    {
+        return *this;
+    }
+}
+
+template<typename T> inline 
+bool IntegralIndex<T>::isValid() const
+{
+    return m_current < m_end;
+}
+
+template<typename T>
+Option<T> IntegralIndex<T>::get() const
+{
+    if( isValid() ) 
+    {
+        return Some( m_current );
+    }
+    else 
+    {
+        return None<T>();
+    }
+}
+
+template<typename T>
+bool operator==( IntegralIndex<T> const& lhs, IntegralIndex<T> const& rhs )
+{
+    return lhs.m_current == rhs.m_current &&
+           lhs.m_end && rhs.m_end &&
+           lhs.m_step && rhs.m_step;
+}
 
 #endif
---
+++ sources/tests/IntegralIndex.cc
@@ -0,0 +1,65 @@
+/**
+ * @file IntegralIndex.cc
+ * @author Massimiliano Pagani (maxpag@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-09-23
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+#include <flic/IntegralIndex.hh>
+#include <cassert>
+
+namespace 
+{
+    void test_integral()
+    {
+        IntegralIndex index{ 3, 7 };
+
+        for( int i=3; i<7; ++i )
+        {
+            assert( index.isValid() );
+            auto next = index.next();
+            assert( index.get().get() == i );
+            index = next;
+        }
+        assert( !index.isValid() );
+    }
+
+    void
+    test_limit()
+    {
+        IntegralIndex index{ 3, 3 };
+        assert( !index.isValid() );
+        auto next = index.next();
+        assert( next == index );
+
+        IntegralIndex invalid{ 3, 2 };
+        assert( !index.isValid() );
+    }
+
+    void
+    test_step(int b, int e)
+    {
+        IntegralIndex index{ b, e, 3 };
+        for( int i=b; i<e; i += 3 )
+        {
+            assert( index.isValid() );
+            assert( index.get().get() == i );
+            index = index.next();
+        }
+        assert( !index.isValid() );
+    }
+}
+
+int main()
+{
+    test_integral();
+    test_limit();
+    test_step(0,9);
+    test_step(0,10);
+    test_step(0,11);
+    return 0;
+}
